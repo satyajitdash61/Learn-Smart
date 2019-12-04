af6703a1d4ce3c9d8dd9f509942a00511d8936bd
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-12-04 16:54:34 +0530

    adding data

--- client/src/Components/Workspace/Workspace.jsx
+++ client/src/Components/Workspace/Workspace.jsx
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 class Workspace extends Component {
-    render() { 
-        return ( 
-            <div>
-                <h1 style={{color :"Blue"}}>Hello</h1>
-            </div>
-         );
-    }
+  state = {
+    Length: ""
+  };
+  componentDidMount() {
+    axios
+      .get("https://nut-case.s3.amazonaws.com/coursessc.json")
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  render() {
+    const Length = this.state;
+    return (
+      <div>
+        <h1>hi</h1>
+        <p>Length : {Length}</p>
+      </div>
+    );
+  }
 }
- 
+
 export default Workspace;
