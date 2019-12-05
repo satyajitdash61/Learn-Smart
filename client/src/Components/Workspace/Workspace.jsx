7885cd8268e5883792764accd9d61001890c3ad8
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-12-05 20:07:26 +0530

    deleting axios

--- client/src/Components/Workspace/Workspace.jsx
+++ client/src/Components/Workspace/Workspace.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import axios from "axios";
 import "./Workspace.css";
 import loadingGIF from "../../Images/preview.gif";
 import Card from "react-bootstrap/Card";
