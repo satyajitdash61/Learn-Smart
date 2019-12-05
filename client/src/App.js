9027385803b3dc2aa24c313c5fbb1276fb785563
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-12-05 20:55:57 +0530

    added comments in app.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
+// importing the React from react
 import React from 'react';
+// importing the bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css';
+// importing the css file
 import './App.css';
+// importing the Workspace component
 import Workspace from './Components/Workspace/Workspace'
 
 function App() {
   return (
     <div className="container-fluid">
+      {/* adding the Workspace component */}
       <Workspace />
     </div>
   );
 }
-
+// exporting
 export default App;
